7c9079d9252167ecdf2b0823ac40116e76c22b25
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-11 07:56:35 +0530

    Calculate Daily Employee Wage

--- FlipCoinSimulator.java
+++ FlipCoinSimulator.java
@@ -6,11 +6,17 @@ public class FlipCoinSimulator {
     public static void main(String[] args) {
         Random rd = new Random();
         int attendance = rd.nextInt(2);
+        int wage_per_hour = 20;
+        int full_day_hour = 8 ;
+        int wage = 0;
         System.out.println(attendance);
         if (attendance == 1 ) {
             System.out.println(" Employee is Present...");
+            wage = full_day_hour * wage_per_hour * attendance;
         } else {
             System.out.println(" Employee is Absent...");
+            wage = full_day_hour * wage_per_hour * attendance;
         }
+        System.out.println(" Employee Daily Wage = "+wage+"₹");
     }
 }
