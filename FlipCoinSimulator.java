dbe4eddc1816251489927adf99a28f2d5de5c9a1
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-11 08:18:47 +0530

    Part Time Feature Added in UC3

--- FlipCoinSimulator.java
+++ FlipCoinSimulator.java
@@ -7,16 +7,25 @@ public class FlipCoinSimulator {
         Random rd = new Random();
         int attendance = rd.nextInt(2);
         int wage_per_hour = 20;
-        int full_day_hour = 8 ;
+        int full_day_hour = 8;
+        int part_time_hour = 3;
         int wage = 0;
         System.out.println(attendance);
-        if (attendance == 1 ) {
-            System.out.println(" Employee is Present...");
-            wage = full_day_hour * wage_per_hour * attendance;
+        if (attendance == 1) {
+            int time =  0 + rd.nextInt(2);
+            System.out.println(time);
+            if (time == 1) {
+                System.out.println(" Employee is Present Full Day...");
+                wage = full_day_hour * wage_per_hour * attendance;
+            } else if (time == 0) {
+                System.out.println(" Employee worked Part Time...");
+                wage = part_time_hour * wage_per_hour * attendance;
+            }
+
         } else {
             System.out.println(" Employee is Absent...");
             wage = full_day_hour * wage_per_hour * attendance;
         }
-        System.out.println(" Employee Daily Wage = "+wage+"₹");
+        System.out.println(" Employee Daily Wage = " + wage + "₹");
     }
 }
