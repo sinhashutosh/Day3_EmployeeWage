7b97f801d56a3a1544c3ec9914b779798dc75a00
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-11 10:09:12 +0530

    Calculate Daily Employee Wage with the help of Switch Case

--- FlipCoinSimulator.java
+++ FlipCoinSimulator.java
@@ -5,27 +5,26 @@ import java.util.Random;
 public class FlipCoinSimulator {
     public static void main(String[] args) {
         Random rd = new Random();
-        int attendance = rd.nextInt(2);
+        int attendance = rd.nextInt(3);
         int wage_per_hour = 20;
         int full_day_hour = 8;
         int part_time_hour = 3;
         int wage = 0;
-        System.out.println(attendance);
-        if (attendance == 1) {
-            int time =  0 + rd.nextInt(2);
-            System.out.println(time);
-            if (time == 1) {
+       // System.out.println(attendance);
+        switch (attendance) {
+            case 0:
+                System.out.println(" Employee is Absent...");
+                wage = full_day_hour * wage_per_hour * attendance;
+                break;
+            case 1:
                 System.out.println(" Employee is Present Full Day...");
                 wage = full_day_hour * wage_per_hour * attendance;
-            } else if (time == 0) {
+                break;
+            case 2:
                 System.out.println(" Employee worked Part Time...");
                 wage = part_time_hour * wage_per_hour * attendance;
-            }
-
-        } else {
-            System.out.println(" Employee is Absent...");
-            wage = full_day_hour * wage_per_hour * attendance;
+                break;
         }
-        System.out.println(" Employee Daily Wage = " + wage + "₹");
+                System.out.println(" Employee Daily Wage = " + wage + "₹");
     }
 }
